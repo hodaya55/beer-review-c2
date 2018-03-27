e9417e7740b2b45d1954f2aff3d12cf02f8fad7e
hodaya55 <hodayah159@gmail.com>
2018-03-27 17:04:29 +0300

    declare addBeer func, add btnClick

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+var addBeer= function (name, category){
+
+}
+
+var btnClick= function(){
+    addBeer($(".beer-input form-control"),$(".category-input form-control"));
+}
+
+$(".post-beer").click(btnClick);
