d0a47dcb15dd8f9ce7462c4a69cf9e368f77ce9f
hodaya55 <hodayah159@gmail.com>
2018-03-27 17:11:52 +0300

    done addBeer func

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 
+var beers=[]; // a global array called beers.
 
 var addBeer= function (name, category){
-
+var beer= {
+    "name-beer": name,
+    "category-beer": category
+         }
+         beers.push(beer);
 }
 
 var btnClick= function(){
-    addBeer($(".beer-input form-control"),$(".category-input form-control"));
+    addBeer($(".beer-input form-control").val(),$(".category-input form-control").val());
 }
 
 $(".post-beer").click(btnClick);
+
+
