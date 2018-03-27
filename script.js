374dce0934593dba3d76eeaa55bf2eedb4205a25
hodaya55 <hodayah159@gmail.com>
2018-03-27 18:37:22 +0300

    adding select elemnt of rate

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 
-var beers=[]; // a global array called beers.
+var beers = []; // a global array called beers.
 
-var addBeer= function (name, category){
-var beer= {
-    "name-beer": name,
-    "category-beer": category
-         }
-         beers.push(beer);
+var addBeer = function (name, category, rate) {
+    var beer = {
+        myName: name,
+        myCategory: category,
+        myRate: rate
+    }
+    beers.push(beer);
 }
 
-var btnClick= function(){
-    addBeer($(".beer-input form-control").val(),$(".category-input form-control").val());
+var btnClick = function () {
+    var n = $(".beer-input.form-control").val();
+    var c = $(".category-input.form-control").val();
+    var r= $("#rate option:selected").text();
+    addBeer(n, c, r);
+    renderBeers();
 }
 
 $(".post-beer").click(btnClick);
 
+
+var renderBeers = function () {
+    $(".beers-list").find("li").remove();
+
+    for (var b in beers)
+        $('.beers-list').append('<li>' + "name: "+beers[b].myName + " ,category: " + beers[b].myCategory + ",rate: "+beers[b].myRate+'</li>');
+    
+}
 
