cb9d6f867ca3932ee37d9a4479ce3657236da3dc
hodaya55 <hodayah159@gmail.com>
2018-04-09 15:26:37 +0300

    use closure and modular design to reorganize our beer-review code

--- script.js
+++ script.js
@@ -1,87 +1,39 @@
 
-var beers = []; // a global array called beers.
 
-var addBeer = function (name, category, rate) {
-    var beer = {
-        myName: name,
-        myCategory: category,
-        myRate: rate
-    }
-    beers.push(beer);
-}
-
-var btnClick = function () {
-    var n = $(".beer-input.form-control").val();
-    var c = $(".category-input.form-control").val();
-    var r = $("#rate option:selected").text();
-    addBeer(n, c, r);
-    renderBeers();
-    // $(".beers-list-sort").find("li").remove();
-}
-
-//$(".post-beer").click(btnClick);
-
-
-var renderBeers = function () {
-    $(".beers-list").find("li").remove();
-    $(".beers-list-sort").find("li").remove();
-
-    for (var b in beers)
-        $('.beers-list').append('<li>' + "name: " + beers[b].myName + " ,category: " + beers[b].myCategory + ",rate: " + beers[b].myRate + '</li>');
-
-}
-
-var bool = false;
-
-var sortASC = function sortByKey(a, b) {
-
-    $(".beers-list-sort").find("li").remove();
-
-    beers = beers.sort(function (a, b) {
-        var x = a["myRate"];
-        var y = b["myRate"];
-        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-    });
-
-
-    if (!bool) {
-        // $("#sortBtn").text('Sort By DES');
-        bool = true;
-    }
-
-    else {
-        // $("#sortBtn").text('Sort By ASC');
-        beers.reverse();
-        bool = false;
-    }
-
-    //display on the page the sorted list 
-    for (var b in beers)
-        $('.beers-list-sort').append('<li>' + "name: " + beers[b].myName + " ,category: " + beers[b].myCategory + ",rate: " + beers[b].myRate + '</li>');
-
-}
-
-
-$("#sortBtn").click(sortASC);
-
-/*
-var BeerReviewApp =function(){
+var BeerReviewApp = function () {
+    var beers = [];
+  
+    var $beerList = $('.beers-list');
+  
+    var updateBeers = function () {
+      $beerList.empty();
+  
+      for (var i = 0; i < beers.length; i += 1) {
+        var beerHTML = "<li>" + beers[i].name + " - " + beers[i].category + " - " + beers[i].rate+"</li>";
+  
+        $beerList.append(beerHTML);
+      }
+    };
+  
+    var addBeer = function (beerName, beerCategory, beerRate) {
+      beers.push({ name: beerName, category: beerCategory, rate: beerRate});
+    };
+  
     return {
-        addBeer: addBeer,
-        renderBeers: renderBeers
+      updateBeers: updateBeers,
+      addBeer: addBeer
     }
-};
-
-var app = BeerReviewApp();
-
-$(".post-beer").on('click', function(){
-    var n = $(".beer-input.form-control").val();
-    var c = $(".category-input.form-control").val();
-    var r = $("#rate option:selected").text();
-    app.addBeer(n, c, r);
-    app.renderBeers();
-});
-*/
+  };
+  
+  var app = BeerReviewApp();
+  
+  $('.post-beer').on('click', function () {
+    var name = $('.beer-input').val();
+    var category = $('.category-input').val();
+    var rate = $("#rate option:selected").text();
+    app.addBeer(name, category, rate);
+    app.updateBeers();
+  });
 
 
 
